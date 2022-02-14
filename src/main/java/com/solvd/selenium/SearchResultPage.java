96fd97f5b58e91e198c2a0d7251e86ee63d8a6d0
Saeid Vahidnia <svahidnia@Saeids-MacBook-Pro.local>
2022-02-14 06:26:24 +0300

    created SearchResultPage

---
+++ src/main/java/com/solvd/selenium/SearchResultPage.java
@@ -0,0 +1,27 @@
+package com.solvd.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class SearchResultPage {
+
+    public WebDriver driver;
+    
+    @FindBy(xpath = "//div[@class='count___1ZIhY gl-body--s']")
+    private List<WebElement> productResult;
+
+    public SearchResultPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+        this.driver.get(driver.getCurrentUrl());
+    }
+
+    public List<WebElement> getProductResult() {
+        return productResult;
+    }
+
+}
